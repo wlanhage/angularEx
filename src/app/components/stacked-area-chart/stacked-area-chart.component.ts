6ff279b543fa80544df1609ddb873c5b4076655d
wlanhage <wlanhage@gmail.com>
2024-11-19 14:05:17 +0100

    deleted testpage

--- src/app/components/stacked-area-chart/stacked-area-chart.component.ts
+++ src/app/components/stacked-area-chart/stacked-area-chart.component.ts
@@ -40,7 +40,7 @@ export class StackedAreaChartComponent implements OnInit {
   }
 
   updateChartSize(): void {
-    const width = window.innerWidth * 0.7;
+    const width = window.innerWidth * 0.75;
     const height = window.innerHeight * 0.7;
     this.view = [width, height];
     this.showLegend = window.innerWidth > 768;
